8bf547f3d428caee387c28d692fc7a66ab3d5391
AntonTrofimov2025 <xerurganton@gmail.com>
2026-02-06 10:05:56 +0100

    made ArmstrongNumberDeterminer

---
+++ ArmstrongNumbers.py
@@ -0,0 +1,22 @@
+print("Число Армстронга")
+number = int(input("Введите число: "))
+q = 10
+buffer = 0
+buffer2 = ""
+
+for i in range(1, number + 1):
+    i = str(i)
+    length = len(i)
+    i = int(i)
+    comp = i
+    while i != 0:
+        res = i % q
+        i = i // q
+        buffer = buffer + res ** length
+    if buffer == comp:
+        buffer = str(buffer)
+        buffer2 = buffer2 + buffer + ", "
+        buffer = int(buffer)
+    buffer = 0
+
+print(buffer2[0: len(buffer2) - 2])
