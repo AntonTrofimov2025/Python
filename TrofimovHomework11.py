58cfbd579c08d46ec9927fbd9c3c8c02d4936852
AntonTrofimov2025 <xerurganton@gmail.com>
2026-02-10 20:00:19 +0100

    Brought few little corrections to HW11

--- TrofimovHomework11.py
+++ TrofimovHomework11.py
@@ -1,28 +1,28 @@
-# # Task I
-#
-# text = "My number is 123-456-789"
-# res = ""
-#
-# for i in text:
-#     if i.isdigit():
-#         res = res + "*"
-#     else:
-#         res = res + i
-#
-# print(res)
-#
-# # Task II
-#
-# text = "Programming in python".lower()
-# buffer = ""
-#
-# for i in range(0, len(text)):
-#     if text.count(text[i]) > 1 and text[i] not in buffer:
-#         times = text.count(text[i])
-#         print(f"Символ '{text[i]}' встречается {times} раз(а)")
-#         buffer = buffer + text[i]
-#     else:
-#         continue
+# Task I
+
+text = "My number is 123-456-789"
+res = ""
+
+for i in text:
+    if i.isdigit():
+        res = res + "*"
+    else:
+        res = res + i
+
+print(res)
+
+# Task II
+
+text = "Programming in python".lower()
+buffer = ""
+
+for i in range(0, len(text)):
+    if text.count(text[i]) > 1 and text[i] not in buffer:
+        times = text.count(text[i])
+        print(f"Символ '{text[i]}' встречается {times} раз(а)")
+        buffer = buffer + text[i]
+    else:
+        continue
 
 # Task III
 
@@ -38,7 +38,7 @@ res = ""
 
 for i in text:
     if i.isdigit() or i.replace(".", "", 1).isdigit():
-        res = res + i.replace(i, str(float(i) * 10)) + " "
+        res = res + str(float(i) * 10) + " "
     else:
         res = res + i + " "
 
