0bbc2e81b40c8cd4f5d3bf35fea2cbed3c9fee63
AntonTrofimov2025 <xerurganton@gmail.com>
2026-02-11 17:00:45 +0100

    Just made new hm №12

---
+++ TrofimovHomework12.py
@@ -0,0 +1,29 @@
+# Task I
+
+numbers = [1245.4435, -302.1403, 87459.99, -520.8265, 1450.001]
+res = 0
+for item in range(0, len(numbers)):
+    if numbers[item] > 0:
+        res = res + numbers[item]
+    else:
+        continue
+
+print(f"Сумма положительных чисел: ${res:,.2f}")
+
+# Task II
+
+data_list = [
+ "John 23 12345.678",
+ "Alice 30 200.50",
+ "Bob 35 15000.3",
+ "Charlie 40 500.75"
+]
+
+buffer = []
+
+for item in range(0, len(data_list)):
+    buffer = data_list[item].split()
+    name = buffer[0]
+    age = buffer[1]
+    summe = float(buffer[2])
+    print("Имя: {0:<10} | Возраст: {1:<3} | Баланс: {2:>10,.2f}".format(name, age, summe))
