9aae5e040f23ddf6fe4f96d1112b5c1089540bb2
AntonTrofimov2025 <xerurganton@gmail.com>
2026-03-02 19:09:48 +0100

    Forgotten to uncomment another rest of HW17

--- TrofimovHomework17.py
+++ TrofimovHomework17.py
@@ -1,57 +1,57 @@
-# """Напишите программу, которая создает список из всех уникальных
-# символов строки, за исключением пробелов."""
-#
-# text = "hello world"
-#
-# # text = text.replace(" ", "")
-# text = "".join(text.split())
-#
-# print(f"Уникальные символы: {set(text)}")
-#
-# """Напишите программу, которая принимает два списка
-# чисел и выводит список, содержащий элементы без
-# повторений, которые присутствуют в обоих списках."""
-#
-# list1 = [1, 2, 3, 4, 5]
-# list2 = [3, 4, 5, 6, 7]
-#
-# print(f"Элементы в обоих списках: {set(list1) & set(list2)}") # set(list1).intersection(set(list2))
-#
-# # Task I
-# "Проверка на подмножество"
-# """Напишите программу, которая проверяет, содержит ли первое множество
-# все элементы второго множества.
-# Реализуйте решение несколькими способами.
-# Решите одним из способов не используя возможности множеств."""
-#
-# set1 = {1, 2, 3, 4}
-# set2 = {2, 3}
-#
-# "With set options"
-# if set1 >= set2:
-#     success = True
-# else:
-#     success = False
-#
-# print("Пример вывода:")
-# print(success)
-#
-# "Without set options"
-# success = True
+"""Напишите программу, которая создает список из всех уникальных
+символов строки, за исключением пробелов."""
+
+text = "hello world"
+
+# text = text.replace(" ", "")
+text = "".join(text.split())
+
+print(f"Уникальные символы: {set(text)}")
+
+"""Напишите программу, которая принимает два списка
+чисел и выводит список, содержащий элементы без
+повторений, которые присутствуют в обоих списках."""
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+print(f"Элементы в обоих списках: {set(list1) & set(list2)}") # set(list1).intersection(set(list2))
+
+# Task I
+"Проверка на подмножество"
+"""Напишите программу, которая проверяет, содержит ли первое множество
+все элементы второго множества.
+Реализуйте решение несколькими способами.
+Решите одним из способов не используя возможности множеств."""
+
+set1 = {1, 2, 3, 4}
+set2 = {2, 3}
+
+"With set options"
+if set1 >= set2:
+    success = True
+else:
+    success = False
+
+print("Пример вывода:")
+print(success)
+
+"Without set options"
+success = True
+for i in set2:
+    if i not in set1:
+        success = False
+
+# success = None
 # for i in set2:
-#     if i not in set1:
+#     if i in set1:
+#         success = True
+#     else:
 #         success = False
-#
-# # success = None
-# # for i in set2:
-# #     if i in set1:
-# #         success = True
-# #     else:
-# #         success = False
-# #         break
-#
-# print("Пример вывода:")
-# print(success)
+#         break
+
+print("Пример вывода:")
+print(success)
 
 # Task II
 
